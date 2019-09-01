2484febf0c9d173453b7bd67ef72e8cb3fbdee25
Arthur Diniz <arthurbdiniz@gmail.com>
2019-09-01 17:54:19 -0300

    Users routes to CRUD
    Closes: #9
    
    Signed-off-by: Arthur Diniz <arthurbdiniz@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :transactions
   resources :clients
   resources :cards
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users
+  
   post 'authenticate', to: 'authentication#authenticate'
 end
